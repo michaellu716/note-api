e45a7c46a7e17f18ea48de50fe6f9d7e464ed21d
Michael Lu <michael_108@hotmail.com>
2019-08-12 15:50:19 +0000

    react notes lesson 12 completed

--- src/components/List.js
+++ src/components/List.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
 class List extends React.Component {
+  componentWillMount() {
+    this.props.getNotes();
+  }
   render() {
     return (
       <div className="list-container">
