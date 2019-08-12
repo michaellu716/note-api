90d60a9e1f8854d488bc9d59bea1b483cb21fa4f
Michael Lu <michael_108@hotmail.com>
2019-08-12 20:37:25 +0000

    finished 17

--- src/helpers/urlFor.js
+++ src/helpers/urlFor.js
@@ -1,5 +1,5 @@
 const urlFor = (endpoint) => {
-  return 'https://note-api-mike-lu.herokuapp.com' + endpoint;
+  return 'https://note-api-mike-lu.herokuapp.com/' + endpoint;
 };
 
 export default urlFor;
